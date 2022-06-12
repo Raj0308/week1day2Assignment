a706391f7eba7dff67798a81de6c7fcf994d07ab
Raj0308 <103851446+Raj0308@users.noreply.github.com>
2022-06-12 13:07:50 +0530

    Add files via upload

---
+++ MissingElementInAnArray.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class MissingElementInAnArray {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,7,6,8};
+		
+		Arrays.sort(arr);
+
+		for (int i=1; i< arr.length-1; i++) {
+			System.out.println(arr[i-1]);
+			if (i!=arr[i-1]) {
+				
+				System.out.println(i);
+				break;
+			}
+			
+		}
+	}
+	
+}
